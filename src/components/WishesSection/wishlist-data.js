9bd5de27ed008503f0c01db82206c476c0df690b
quynhdv <quynhdv@vng.com.vn>
2023-01-07 12:35:50 +0700

    edit images

--- src/components/WishesSection/wishlist-data.js
+++ src/components/WishesSection/wishlist-data.js
@@ -1,12 +1,11 @@
 import Zakiy from '@assets/images/w-zakiy.jpg';
-import Moletta from '@assets/images/w-moletta.jpg';
 import hapsari from '@assets/images/w-hapsari.jpg';
 import dianara from '@assets/images/w-dianara.jpg';
 import rahma from '@assets/images/w-rahma.jpg';
 import iris from '@assets/images/iris.jpg';
 import fajar from './assets/w-fajar.jpg';
 import risa from './assets/w-risa.jpg';
-import ghea from './assets/w-ghea.jpg';
+import ghea from './assets/w-fajar.png';
 
 export const wishlist = [
   {
@@ -17,7 +16,7 @@ export const wishlist = [
   },
   {
     name: 'Tín',
-    infoName: 'IGC',
+    infoName: 'VMG',
     description: `Chúc mừng tân lang - tân nương điển trai xinh đẹp của tớ. Chúc 2 cậu sẽ có một khởi đầu mới thuận lợi, cùng nhau xây đắp một mái ấm hạnh phúc mà hai cậu vẫn ao ước nhé. Happy Wedding!`,
     image: risa,
   },
@@ -58,7 +57,7 @@ export const wishlist = [
     image: rahma,
   },
   {
-    name: 'Nguyễn Văn Lực',
+    name: 'Thành Thân',
     infoName: 'Lixil',
     description: `Thật vui vì cuối cùng ngày này cũng tới với bạn. Cuối cùng thì bạn đã dừng chân tại bến đỗ, mảnh ghép hoàn hảo của cuộc đời mình. Tớ không biết nói gì hơn, thành tâm chúc hai bạn hạnh phúc và hạnh phúc, sống một cuộc sống vui vẻ cùng nhau mãi mãi.`,
     image: ghea,

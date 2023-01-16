0ac80bb6059f18c853dd412c791f4cb92f86ec74
quynhdv <quynh.dinh@primedata.ai>
2023-01-16 16:34:03 +0700

    shuffle music

--- src/components/FloatingMusic/index.js
+++ src/components/FloatingMusic/index.js
@@ -1,13 +1,16 @@
 import React, { Fragment, useState } from 'react';
 import IconMusic from '@assets/images/music-icon.png';
 import IconMusicStop from '@assets/images/music-stop-icon.png';
-import MusicBacksound from '@assets/music/lagu-pernikahan-impian.mp3';
+import ngaydautien from '@assets/music/ngaydautien.mp3';
+import thichthich from '@assets/music/thichthich.mp3';
+import thuongthuong from '@assets/music/thuongthuong.mp3';
 
 import { styMusicFloating } from './styles';
 
 function FloatingMusic() {
   const [play, setPlay] = useState(true);
-
+  const songs = [ngaydautien, thichthich, thuongthuong]
+  const song = songs[Math.floor(Math.random() * songs.length)];
   const toggleMusic = () => {
     const myAudio = document.getElementById('myAudio');
     /**
@@ -27,7 +30,7 @@ function FloatingMusic() {
     <Fragment>
       <div css={styMusicFloating}>
         <audio id="myAudio" loop className="hide">
-          <source src={MusicBacksound} type="audio/mpeg" />
+          <source src={song} type="audio/mpeg" />
           Your browser does not support the audio element.
         </audio>
 

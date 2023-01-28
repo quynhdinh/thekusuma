9b74523e40f1253201b198a7510544fbf7c77692
quynhdv <quynh.dinh@primedata.ai>
2023-01-28 22:13:31 +0700

    bambu's friends & remove old wishes

--- src/components/FooterSection/index.js
+++ src/components/FooterSection/index.js
@@ -31,7 +31,7 @@ function FooterSection({ isInvitation }) {
           <div className="row copyright">
             <div className="col-md-12 text-center">
               <p>
-                <small className="block">&copy; 2020 Bình & Vân Wedding. All Rights Reserved.</small>
+                <small className="block">&copy; 2023 Bình & Vân Wedding. All Rights Reserved.</small>
                 <small className="block">
                   Covid-19 Icon by{' '}
                   <a href="https://www.flaticon.com/packs/covid-protection-measures-5" target="_blank" rel="noreferrer">
--- src/components/WishesSection/wishlist-data.js
+++ src/components/WishesSection/wishlist-data.js
@@ -7,60 +7,75 @@ import iris from '@assets/images/iris.jpg';
 import fajar from './assets/w-fajar.jpg';
 import risa from './assets/w-risa.jpg';
 import ghea from './assets/w-fajar.png';
+import thanh from './assets/thanh.jpg';
+import huong from './assets/huong.jpg';
+import trang from './assets/trang.jpg';
 
 export const wishlist = [
   {
-    name: 'Phước Nguyễn',
-    infoName: 'Publicis Groupe',
-    description: `Thật vui vì cuối cùng ngày này cũng tới với bạn. Cuối cùng thì bạn đã dừng chân tại bến đỗ, mảnh ghép hoàn hảo của cuộc đời mình. Tớ không biết nói gì hơn, thành tâm chúc hai bạn hạnh phúc và hạnh phúc, sống một cuộc sống vui vẻ cùng nhau mãi mãi.`,
-    image: Zakiy,
+    name: 'Bạn Thanh',
+    infoName: '',
+    description: `Thế là con ngựa hoang thứ 3 của tụi này đã bị Mr Bình thuần hoá rồi, chúc mừng cưng đã vượt qua deadline “hối cưới” của năm nay, luôn vui tươi,hạnh phúc,hôn nhân viên mãn nhé baby! ❤️❤️❤️❤️. Gửi bạn Vân yêu dấu nhé`,
+    image: thanh,
   },
   {
-    name: 'Lương Minh Tín',
-    infoName: 'Trung tâm VMG',
-    description: "Chúc mừng tân lang - tân nương điển trai xinh đẹp của tớ. Chúc 2 cậu sẽ có một khởi đầu mới thuận lợi, cùng nhau xây đắp một mái ấm hạnh phúc mà hai cậu vẫn ao ước nhé. Happy Wedding!",
-    image: risa,
+    name: 'Bạn Trang',
+    infoName: '',
+    description: `Chúc mừng Anh Bình Koc đã may mắn có được Ngựa Hoang Vân yêu dấu. Hãy nắm chặt tay nhau đến lúc già nhé.`,
+    image: trang,
   },
   {
-    name: 'Nguyễn Hải Âu',
-    infoName: 'Sinh viên',
-    description: `Người xưa có nói "Tu 10 năm mới chung chuyến đò, tu 100 năm mới chung chăn gối". Đến với nhau là do duyên phận, còn hạnh phúc đến trọn đời do hai người cùng vun đắp. Các em hãy cùng nhau xây dựng một tổ ấm và bên nhau đến "Đầu bạc răng long" mà thôi phải nói là "Tóc nhuộm răng giả" chứ bây giờ già vẫn cần đẹp :D nhỉ Hãy nhớ câu "Thuận vợ thuận chồng tát biển Đông cũng cạn". Chúc đôi uyên ương có một lễ cưới trang trọng, đầm ấm đánh dấu bước khởi đầu của con đường Hạnh Phúc`,
-    image: haiau,
+    name: 'Bạn Hương',
+    infoName: '',
+    description: `Cứ phải gọi là mừng vỡ oà khi nghe tin có người hốt con bạn lâu năm. Chúc mừng anh Bình Koc đã may mắn có được con ngựa quý 😝. Trăm năm hạnh phúc nha 2 bạn Trẻ.`,
+    image: huong,
   },
+  // {
+  //   name: 'Phước Nguyễn',
+  //   infoName: 'Publicis Groupe',
+  //   description: `Thật vui vì cuối cùng ngày này cũng tới với bạn. Cuối cùng thì bạn đã dừng chân tại bến đỗ, mảnh ghép hoàn hảo của cuộc đời mình. Tớ không biết nói gì hơn, thành tâm chúc hai bạn hạnh phúc và hạnh phúc, sống một cuộc sống vui vẻ cùng nhau mãi mãi.`,
+  //   image: Zakiy,
+  // },
+  // {
+  //   name: 'Lương Minh Tín',
+  //   infoName: 'Trung tâm VMG',
+  //   description: "Chúc mừng tân lang - tân nương điển trai xinh đẹp của tớ. Chúc 2 cậu sẽ có một khởi đầu mới thuận lợi, cùng nhau xây đắp một mái ấm hạnh phúc mà hai cậu vẫn ao ước nhé. Happy Wedding!",
+  //   image: risa,
+  // },
   {
     name: 'Dexter',
     infoName: 'Bệnh viện Đa Khoa Đồng Nai',
-    description: `Hôn nhân ko phải là nơi thuyền tình cập bến, mà là nơi 2 người yêu nhau quyết định cùng giăng buồm vượt sóng ra khơi”. Chúc mừng hai bạn đã không còn đơn phương trong hành trình hạnh phúc này!`,
+    description: `Hôn nhân ko phải là nơi thuyền tình cập bến, mà là nơi 2 người yêu nhau quyết định cùng giăng buồm vượt sóng ra khơi”. Chúc mừng hai anh chị đã không còn đơn phương trong hành trình hạnh phúc này!`,
     image: hapsari,
   },
   {
     name: 'Iris Hoàng',
     infoName: 'IGC',
-    description: `Chúc Bình ❤ Vân về chung 1 nhà. Không chúc đầu bạc răng long, nhỡ 35-40t tóc bạc răng rụng thì k ổn nên chúc sống bên nhau trọn đời trọn kiếp và thật hạnh phúc nha 2 đứa :D`,
+    description: `Chúc Bình ❤ Vân về chung 1 nhà. Không chúc đầu bạc răng long, nhỡ 35-40t tóc bạc răng rụng thì không ổn nên chúc sống bên nhau trọn đời trọn kiếp và thật hạnh phúc nha 2 đứa :D`,
     image: iris,
   },
   {
     name: 'Mai Ngũ',
     infoName: 'ABC School',
-    description: `Chúc cho Bình và Vân nên đôi vợ chồng Chúc cho hai bạn mặn nồng tình thương Thương nhau thương tới tận già Thương cho tới lúc bạc cha mái đầu.`,
+    description: `Chúc cho Bình và Vân nên đôi vợ chồng Chúc cho hai anh chị mặn nồng tình thương Thương nhau thương tới tận già Thương cho tới lúc bạc cha mái đầu.`,
     image: dianara,
   },
   {
     name: 'Quỳnh',
     infoName: 'Zalo',
-    description: `Thật vui vì cuối cùng ngày này cũng tới với bạn. Cuối cùng thì bạn đã dừng chân tại bến đỗ, mảnh ghép hoàn hảo của cuộc đời mình. Tớ không biết nói gì hơn, thành tâm chúc hai bạn hạnh phúc và hạnh phúc, sống một cuộc sống vui vẻ cùng nhau mãi mãi.`,
+    description: "Chúc mừng tân lang - tân nương điển trai xinh đẹp của em. Chúc 2 cậu sẽ có một khởi đầu mới thuận lợi, cùng nhau xây đắp một mái ấm hạnh phúc mà hai cậu vẫn ao ước nhé. Happy Wedding!",
     image: fajar,
   },
   {
     name: 'Bé Xíu',
     infoName: 'Akzonobel',
-    description: `Thật vui vì cuối cùng ngày này cũng tới với bạn. Cuối cùng thì bạn đã dừng chân tại bến đỗ, mảnh ghép hoàn hảo của cuộc đời mình. Tớ không biết nói gì hơn, thành tâm chúc hai bạn hạnh phúc và hạnh phúc, sống một cuộc sống vui vẻ cùng nhau mãi mãi.`,
+    description: `Thật vui vì cuối cùng ngày này cũng tới với hai anh chị. Cuối cùng thì cả hai đã dừng chân tại bến đỗ, mảnh ghép hoàn hảo của cuộc đời mình. Em không biết nói gì hơn, thành tâm chúc hai anh chị hạnh phúc và hạnh phúc, sống một cuộc sống vui vẻ cùng nhau mãi mãi.`,
     image: rahma,
   },
   {
     name: 'Đức Nguyễn',
     infoName: 'CP',
-    description: `Thật vui vì cuối cùng ngày này cũng tới với bạn. Cuối cùng thì bạn đã dừng chân tại bến đỗ, mảnh ghép hoàn hảo của cuộc đời mình. Tớ không biết nói gì hơn, thành tâm chúc hai bạn hạnh phúc và hạnh phúc, sống một cuộc sống vui vẻ cùng nhau mãi mãi.`,
+    description: `Người xưa có nói "Tu 10 năm mới chung chuyến đò, tu 100 năm mới chung chăn gối". Đến với nhau là do duyên phận, còn hạnh phúc đến trọn đời do hai người cùng vun đắp. Các em hãy cùng nhau xây dựng một tổ ấm và bên nhau đến "Đầu bạc răng long" mà thôi phải nói là "Tóc nhuộm răng giả" chứ bây giờ già vẫn cần đẹp :D nhỉ Hãy nhớ câu "Thuận vợ thuận chồng tát biển Đông cũng cạn". Chúc đôi uyên ương có một lễ cưới trang trọng, đầm ấm đánh dấu bước khởi đầu của con đường hạnh phúc`,
     image: ghea,
   },
 ];
